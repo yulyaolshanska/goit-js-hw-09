c0146df75031f8daa762ee7479728d6e07e4ad2a
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-05-08 13:51:18 +0300

    Задание 1
    1.Переключатель цветов.
    2.Стилизация кнопок.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    startBtn: document.querySelector("[data-start]"),
+    stopBtn: document.querySelector("[data-stop]"),
+    body: document.querySelector('body'),
 
+}
+
+let timerId = null;
+
+refs.startBtn.addEventListener("click", onStartSwitcher);
+refs.stopBtn.addEventListener("click", onStopSwitcher);
+
+console.log(refs.startBtn)
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartSwitcher() {
+    refs.startBtn.setAttribute("disabled", true);
+    refs.stopBtn.removeAttribute("disabled");
+    timerId = setInterval(() => refs.body.style.backgroundColor = getRandomHexColor(), 1000);
+
+
+}
+
+function onStopSwitcher() {
+    clearInterval(timerId);
+    refs.startBtn.removeAttribute("disabled");
+    refs.stopBtn.setAttribute("disabled", true);
+}
