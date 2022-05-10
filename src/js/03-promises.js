d052834affd80b047c17fa855e72213ac5326221
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-05-10 18:04:14 +0300

    Задание 3 - генератор промисов

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,54 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  submitEl: document.querySelector('.form'),
+  delayEl: document.querySelector('[name="delay"]'),
+  stepEl: document.querySelector('[name="step"]'),
+  amountEl: document.querySelector('[name="amount"]'),
+
 }
+  
+const { submitEl, delayEl, stepEl, amountEl } = refs;
+submitEl.addEventListener("submit", onSubmit);
+
+function createPromise(position, delay) {
+   return new Promise((resolve, reject) => {
+     const shouldResolve = Math.random() > 0.3;
+     setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay)
+  })
+};
+
+function generatePromises(delay, step, amount) {
+    for (let i = 1; i <= amount; i += 1) {
+      createPromise(i, delay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`,{
+    timeout: 10000,
+  });
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`,{
+    timeout: 10000,
+  });
+        });
+      delay += step;
+    }
+  }
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const delay = delayEl.value;
+  const step = stepEl.value;
+  const amount = amountEl.value; 
+  generatePromises(delay, step, amount);
+}
+
+
+
